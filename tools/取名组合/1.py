548d973bd52ae04251e2d9c772f18772e5c5f79f
LasterSmithKim <kingone@yeah.net>
2018-11-12 23:26:09 +0800

    文字组合

---
+++ tools/取名组合/1.py
@@ -0,0 +1,40 @@
+from pypinyin import pinyin, lazy_pinyin, Style
+import os
+
+
+def read_file_as_str():
+    file_path = os.path.join('basedata.txt')
+    all_the_text = open(file_path).read()
+    return all_the_text
+
+def style3(ming):
+    pinyinstr = str(pinyin(ming,style=Style.TONE3)[0][0])
+    if "4" in pinyinstr:
+        return 4
+    elif '3' in pinyinstr:
+        return 3
+    elif '2' in pinyinstr:
+        return 2
+    elif '1' in pinyinstr:
+        return 1
+    else:
+        return 0
+
+if __name__ == "__main__":
+    strs = str(read_file_as_str())
+    count = 0
+    num = len(strs)
+    for i in range(0, len(strs)):
+        ming = strs[i][0]
+        datastr=("金"+ming+"  "+str(pinyin(ming)[0][0])+"  1" + str(style3(ming)))
+        # 写入文件
+        file = os.path.join('to-data1.txt')
+        with open(file, 'a+') as f:
+            f.write(datastr + '\n')
+        # 显示写入进度
+        count += 1
+        print(str(round((count / num) * 100, 2)) + str("%"))
+
+
+
+
---
+++ tools/取名组合/2.py
@@ -0,0 +1,44 @@
+from pypinyin import pinyin, lazy_pinyin, Style
+import os
+import subprocess
+
+
+def read_file_as_str():
+    file_path = os.path.join('basedata.txt')
+    all_the_text = open(file_path).read()
+    return all_the_text
+def style3(ming):
+    pinyinstr = str(pinyin(ming,style=Style.TONE3)[0][0])
+    if "4" in pinyinstr:
+        return 4
+    elif '3' in pinyinstr:
+        return 3
+    elif '2' in pinyinstr:
+        return 2
+    elif '1' in pinyinstr:
+        return 1
+    else:
+        return 0
+
+if __name__ == "__main__":
+    strs = str(read_file_as_str())
+    count = 0
+    num = len(strs)*len(strs)
+    for j in range(0, len(strs)):
+        for i in range(0, len(strs)):
+            ming1 = strs[j][0]
+            ming2 = strs[i][0]
+            datastr = ("金"+ming1+ming2+"  "+ str(pinyin("金")[0][0]) +"  "
+                  +str(pinyin(ming1)[0][0])+"  " +str(pinyin(ming2)[0][0])
+                  + "  1" + str(style3(ming1))+ str(style3(ming2)))
+            #写入文件
+            file = os.path.join('to-data2.txt')
+            with open(file, 'a+') as f:
+                f.write(datastr + '\n')
+            #显示写入进度
+            count += 1
+            print(str(round((count/num)*100,2)) +str("%"))
+
+
+
+
